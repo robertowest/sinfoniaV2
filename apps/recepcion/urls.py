0ed165e59d601b3c513856df76a922fd889bb90e
robertowest <roberto.west@gmail.com>
2024-02-12 19:21:44 +0100

    modificado

--- apps/recepcion/urls.py
+++ apps/recepcion/urls.py
@@ -5,5 +5,6 @@ from apps.recepcion import views
 app_name = __package__.split('.')[1]    # en template: request.resolver_match.app_name
 
 urlpatterns = [
-    path('', views.index, name='home'),
+    path('', views.index, name='index'),
+    path('remesas/', views.obtener_remesas, name='remesas'),
 ]
--- apps/recepcion/views.py
+++ apps/recepcion/views.py
@@ -1,6 +1,28 @@
+import requests
 from django.shortcuts import render
+
+from .models import Remesa
 
 
 def index(request):
     context = {}
     return render(request, 'recepcion/index.html', context)
+
+
+# def obtener_remesas(request):
+#     response = requests.get('https://randomuser.me/api')
+#     if response.status_code == 200:
+#         remesas = response.json()
+#     else:
+#         remesas = []
+#     return render(request, 'recepcion/remesas.html', {'remesas': remesas})
+def obtener_remesas(request):
+    # obtenemos un json
+    titles = {'data': [{'id': '20ad5d9c-b32e-4599-8866-a3aaa5ac77de','name': 'name_1'},
+                       {'id': '7b6d76cc-86cd-40f8-be90-af6ced7fec44','name': 'name_2'},
+                       {'id': 'b8843b1a-9eb0-499f-ba64-25e436f04c4b','name': 'name_3'}]}
+
+    # Create a Django model object for each object in the JSON 
+    for title in titles['data']:
+        Remesa.objects.create(id=title['id'], name=title['name'])
+    return render(request, 'recepcion/remesas.html', {'remesas': Remesa.objects.all()})
